cec9aaee26c19fbee77e232b7c9dbd7195418f78
longfellow <longfellow.wang@gmail.com>
2022-12-08 14:41:27 +0800

    最大公约数算法（gcd）

---
+++ gcd/gcd.h
@@ -0,0 +1,13 @@
+#ifndef _DSACXX_GCD_GCD_H_
+#define _DSACXX_GCD_GCD_H_
+
+#define ABS(x) ((0 < (x)) ? (x) : (-(x)))
+
+#include <iostream>
+#include <cmath>
+
+__int64_t gcdCN()
+{
+}
+
+#endif
---
+++ gcd/main.cpp
@@ -0,0 +1,8 @@
+#include "gcd.h"
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    std::cout << " gcd test " << std::endl;
+    return 0;
+}
